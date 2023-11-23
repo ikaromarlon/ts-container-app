570ee967cb3dd18ab6adee6c40df3a253ccb272a
Icaro Silva <ikaromarlon@gmail.com>
2023-11-23 13:02:19 +0100

    build: remove terraform form repository

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-  profile = "terraform"
-}
-
-resource "aws_ecr_repository" "ts_container_aws" {
-  name = "ts-container-aws"
-  image_tag_mutability = "MUTABLE"
-}
