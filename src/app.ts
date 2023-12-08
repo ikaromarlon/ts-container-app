9e3742e56954abaa59bafbfc8234f88ae6478f7f
Icaro Silva <ikaromarlon@gmail.com>
2023-12-08 17:48:39 +0100

    feat: add route for fruits

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,13 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'hello world' });
 });
 
+app.get('/fruits', (req: Request, res: Response) => {
+  res.json([
+    'tomato',
+    'banana',
+    'avocado',
+    'grape'
+  ]);
+});
+
 export default app
