0f69ddfa251c72a30bbc16efa38524a4b6ce380d
Icaro Silva <ikaromarlon@gmail.com>
2023-12-08 10:52:33 +0100

    infra: add ecs

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,15 +1,15 @@
 locals {
   ecr_repository_name = "typescript-aws-container-demo"
 
-  ecs_cluster_name             = "typescript-aws-container-demo-cluster"
-  ecs_task_famliy              = "typescript-aws-container-demo-task"
-  ecs_task_name                = "typescript-aws-container-demo-task"
-  ecs_task_execution_role_name = "typescript-aws-container-demo-task-execution-role"
-  esc_service_name             = "typescript-aws-container-demo-service"
+  ecs_cluster_name             = "tsacd-cluster"
+  ecs_task_famliy              = "tsacd-task"
+  ecs_task_name                = "tsacd-task"
+  ecs_task_execution_role_name = "tsacd-task-execution-role"
+  esc_service_name             = "tsacd-service"
 
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   container_port     = 3000
 
-  application_load_balancer_name = "typescript-aws-container-demo-alb"
-  target_group_name              = "typescript-aws-container-demo-alb-tg"
+  application_load_balancer_name = "tsacd-alb"
+  target_group_name              = "tsacd-alb-tg"
 }
--- infra/main.tf
+++ infra/main.tf
@@ -16,19 +16,19 @@ module "ecrRepository" {
   ecr_repository_name = local.ecr_repository_name
 }
 
-# module "ecsCluster" {
-#   source = "./modules/ecs"
+module "ecsCluster" {
+  source = "./modules/ecs"
 
-#   ecs_cluster_name             = local.ecs_cluster_name
-#   ecs_task_famliy              = local.ecs_task_famliy
-#   ecs_task_name                = local.ecs_task_name
-#   ecs_task_execution_role_name = local.ecs_task_execution_role_name
-#   esc_service_name             = local.esc_service_name
-#   ecr_repo_url                 = module.ecrRepository.repository_url
+  ecs_cluster_name             = local.ecs_cluster_name
+  ecs_task_famliy              = local.ecs_task_famliy
+  ecs_task_name                = local.ecs_task_name
+  ecs_task_execution_role_name = local.ecs_task_execution_role_name
+  esc_service_name             = local.esc_service_name
+  ecr_repo_url                 = module.ecrRepository.repository_url
 
-#   availability_zones = local.availability_zones
-#   container_port     = local.container_port
+  availability_zones = local.availability_zones
+  container_port     = local.container_port
 
-#   application_load_balancer_name = local.application_load_balancer_name
-#   target_group_name              = local.target_group_name
-# }
+  application_load_balancer_name = local.application_load_balancer_name
+  target_group_name              = local.target_group_name
+}
