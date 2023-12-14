3981f9b72170467ab8fc36586198bc82c1146f99
Icaro Silva <ikaromarlon@gmail.com>
2023-12-14 19:36:05 +0100

    infra: move infra to another repo (see readme.md)

--- infra/modules/ecr/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "ecr_repository" {
-  name                 = var.ecr_repository_name
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- infra/modules/ecr/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "ecr_repository_name" {
-  description = "ECR Repo Name"
-  type        = string
-}
